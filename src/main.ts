c3e006786bfdf1174673bda8fa0096af1c75509a
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-16 08:12:15 -0600

    fix: Update the links in the nav bar

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,11 @@ import { Tile } from './types'
 const appContainer = document.querySelector<HTMLDivElement>('#app')!
 appContainer.innerHTML = `
   <h1>SwiftRPG Map</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <a href="https://rpg.swiftirc.net/" target="_blank">Home</a>
+  <a href="https://rpg.swiftirc.net/hiscores" target="_blank">HiScores</a>
 `
 
-const tileDataRequest = await axios.get('http://127.0.0.1:8000/api/tiles')
+const tileDataRequest = await axios.get('http://localhost:8000/api/tiles')
 const tileData = tileDataRequest.data as Tile[]
 
 export class App {
