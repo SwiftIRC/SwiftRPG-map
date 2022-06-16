e28c9792af95200fd494c93d370bd913cad78433
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-16 15:42:00 -0600

    feat: Nginx compatibility

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,8 @@ import './style.css'
 
 import { GameCanvas } from './GameCanvas'
 
-import axios, { Axios } from 'axios'
+import axios from 'axios'
 import { Tile } from './types'
-
-const appContainer = document.querySelector<HTMLDivElement>('#app')!
-appContainer.innerHTML = `
-  <h1>SwiftRPG Map</h1>
-  <a href="https://rpg.swiftirc.net/" target="_blank">Home</a>
-  <a href="https://rpg.swiftirc.net/hiscores" target="_blank">HiScores</a>
-`
-
-const tileDataRequest = await axios.get('http://localhost:8000/api/tiles')
-const tileData = tileDataRequest.data as Tile[]
 
 export class App {
   canvas: GameCanvas
@@ -26,5 +16,19 @@ export class App {
   }
 }
 
-const app = new App(appContainer, document, tileData)
-app.canvas.draw()
+async function main(): Promise<void> {
+  const appContainer = document.querySelector<HTMLDivElement>('#app')!
+  appContainer.innerHTML = `
+  <h1>SwiftRPG Map</h1>
+  <a href="https://rpg.swiftirc.net/" target="_blank">Home</a>
+  <a href="https://rpg.swiftirc.net/hiscores" target="_blank">HiScores</a>
+`
+
+  const tileDataRequest = await axios.get('/api/tiles')
+  const tileData = tileDataRequest.data as Tile[]
+
+  const app = new App(appContainer, document, tileData)
+  app.canvas.draw()
+}
+
+main();
