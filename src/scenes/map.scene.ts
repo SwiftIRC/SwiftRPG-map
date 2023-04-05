e0194106129d410332e2f0f4e39a70dfc41eb0d5
KyleTryon <git@techsquidtv.com>
2023-04-04 22:12:15 -0400

    feat: add roads

--- src/scenes/map.scene.ts
+++ src/scenes/map.scene.ts
@@ -14,8 +14,7 @@ export class MapScene extends Phaser.Scene {
     this.gridManager = new GridManager();
   }
 
-  preload() {
-  }
+  preload() {}
 
   init(tiles: Tile[]): void {
     console.log("Map Scene Init");
@@ -43,26 +42,85 @@ export class MapScene extends Phaser.Scene {
     this.input.on("pointermove", (_pointer: Phaser.Input.Pointer) =>
       this.inputManager.onMouseMove(this.cameras.main, _pointer)
     );
+    this._drawRoads();
     this._drawTrees();
   }
 
   private _drawTrees(): void {
     this._tiles.forEach((tile) => {
       tile.treeMap.forEach((tree) => {
+        this.add.sprite(
+          tile.x + tree.x - tile.width / 2,
+          tile.y + tree.y - tile.height / 2,
+          "tree-trunk"
+        );
+        if (!tree.is_cut) {
           this.add.sprite(
             tile.x + tree.x - tile.width / 2,
             tile.y + tree.y - tile.height / 2,
-            "tree-trunk"
+            "tree-top"
           );
-          if (!tree.is_cut) {
-            this.add.sprite(
-              tile.x + tree.x - tile.width / 2,
-              tile.y + tree.y - tile.height / 2,
-              "tree-top"
-            );
-          }
+        }
+      });
+    });
+  }
+
+  private _drawRoads(): void {
+    const roadColor = 0xa19784;
+    const roadWidth = 12;
+    this._tiles.forEach((tile) => {
+      let hasRoad = false;
+      if (tile.north.is_road) {
+        this.add.rectangle(
+          tile.x + tile.width / 2,
+          tile.y,
+          roadWidth,
+          tile.height,
+          roadColor
+        );
+        hasRoad = true;
       }
-      );
-    })
+      if (tile.east.is_road) {
+        this.add.rectangle(
+          tile.x + tile.width,
+          tile.y + tile.height / 2,
+          tile.width,
+          roadWidth,
+          roadColor
+        );
+        hasRoad = true;
+      }
+      if (tile.south.is_road) {
+        this.add.rectangle(
+          tile.x + tile.width / 2,
+          tile.y + tile.height,
+          roadWidth,
+          tile.height,
+          roadColor
+        );
+        hasRoad = true;
+      }
+      if (tile.west.is_road) {
+        this.add.rectangle(
+          tile.x,
+          tile.y + tile.height / 2,
+          tile.width,
+          roadWidth,
+          roadColor
+        );
+        hasRoad = true;
+      }
+      if (hasRoad) {
+        // creates a cleaner junction
+        this.add.rectangle(
+          tile.x + tile.width / 2,
+          tile.y + tile.height / 2,
+          roadWidth,
+          roadWidth,
+          roadColor
+        );
+      }
+    });
   }
 }
+
